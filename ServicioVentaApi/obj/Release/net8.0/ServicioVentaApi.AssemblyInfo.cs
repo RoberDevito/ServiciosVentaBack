700f07d92c2b3c70be67d5d0325e981179096d92
wyn-code <brunoo6.massocco@gmail.com>
2025-09-09 20:49:51 -0300

    a

--- ServicioVentaApi/obj/Release/net8.0/ServicioVentaApi.AssemblyInfo.cs
+++ ServicioVentaApi/obj/Release/net8.0/ServicioVentaApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ServicioVentaApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f8ae8934bc6c28c3541656a9661dcee122732352")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fcc97c81dceaddd7ffa2eb1bef398503e34dbcd1")]
 [assembly: System.Reflection.AssemblyProductAttribute("ServicioVentaApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ServicioVentaApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

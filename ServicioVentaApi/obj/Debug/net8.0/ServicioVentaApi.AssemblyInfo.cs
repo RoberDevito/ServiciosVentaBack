9ab1340622a1056ac2a509a982039221c949e849
wyn-code <brunoo6.massocco@gmail.com>
2025-09-10 17:27:18 -0300

    Se agrego el gitignore

--- ServicioVentaApi/obj/Debug/net8.0/ServicioVentaApi.AssemblyInfo.cs
+++ ServicioVentaApi/obj/Debug/net8.0/ServicioVentaApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ServicioVentaApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8f9a0bdbb7338c8b16a6a96aa83bcaf3ad4a6681")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f8c4d487512960c765c83f2ed958ed3511e97498")]
 [assembly: System.Reflection.AssemblyProductAttribute("ServicioVentaApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ServicioVentaApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
